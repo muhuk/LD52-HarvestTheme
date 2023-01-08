28a5cf98c8507a13a3a5cf531447fdd656ab2e87
Atamert Ölçgen <muhuk@muhuk.com>
2023-01-08 03:04:14 +0300

    Implement building ships.

--- Constants.gd
+++ Constants.gd
@@ -4,3 +4,5 @@ class_name Constants
 enum {PHASE_GATHER, PHASE_EXECUTE, PHASE_DEFEND}
 
 enum {LB_LABOR, LB_AI, LB_ADMIN}
+
+const SHIP_COST: Array = [5, 3, 2]
--- scenes/MainScreen.gd
+++ scenes/MainScreen.gd
@@ -5,8 +5,11 @@ onready var camp_controls_positions: Node2D = $Background/CampControlsPositions
 onready var world_map: Node2D = $Background/WorldMap
 
 onready var base_info_container: Control = $HUD/BaseInfo
+onready var build_menu_container: Control = $HUD/BuildMenu
 
 onready var end_turn_button: Button = $HUD/HudContainer/MarginContainer/EndTurnButton
+onready var build_ship_button: Button = $HUD/BuildMenu/CenterContainer/PanelContainer/VBoxContainer/BuildShipButton
+onready var ship_cost_info: Label = $HUD/BuildMenu/CenterContainer/PanelContainer/VBoxContainer/ShipCostInfo
 
 # gather, execute, defend
 export var gather_color_inactive: Color
@@ -24,14 +27,20 @@ var phase_phase: float = 0.0
 var phase_phase_speed: float = 2.0
 
 func _ready():
+    randomize()
     setup_game_state()
     create_bases()
-    randomize()
+    ship_cost_info.text = "Ship cost %d labor, %d AI, %d admin." % Constants.SHIP_COST
     call_deferred("on_phase_changed")
     call_deferred("on_strength_changed")
 
 func _process(delta):
     pulse_phase(delta)
+
+func can_build_ship():
+    return game_state.labor_available >= Constants.SHIP_COST[0] and \
+        game_state.ai_available >= Constants.SHIP_COST[1] and \
+        game_state.admin_available >= Constants.SHIP_COST[2]
 
 func create_bases():
     for pos in camp_marker_positions.get_children():
@@ -103,18 +112,28 @@ func on_phase_changed():
             $HUD/HudContainer/PhasePanel/PanelContainer/VBoxContainer/PhaseGather.self_modulate = gather_color_active
             $HUD/HudContainer/PhasePanel/PanelContainer/VBoxContainer/PhaseExecute.self_modulate = execute_color_inactive
             $HUD/HudContainer/PhasePanel/PanelContainer/VBoxContainer/PhaseDefend.self_modulate = defend_color_inactive
+            end_turn_button.disabled = true
+            unfreeze_end_turn_button()
+            base_info_container.visible = true
         Constants.PHASE_EXECUTE:
             for base in base_info_container.get_children():
                 base.disable_harvesting()
             $HUD/HudContainer/PhasePanel/PanelContainer/VBoxContainer/PhaseGather.self_modulate = gather_color_inactive
             $HUD/HudContainer/PhasePanel/PanelContainer/VBoxContainer/PhaseExecute.self_modulate = execute_color_active
             $HUD/HudContainer/PhasePanel/PanelContainer/VBoxContainer/PhaseDefend.self_modulate = defend_color_inactive
+            end_turn_button.disabled = true
+            unfreeze_end_turn_button()
+            base_info_container.visible = false
+            build_menu_container.visible = true
+            build_ship_button.disabled = not can_build_ship()
         Constants.PHASE_DEFEND:
             for base in base_info_container.get_children():
                 base.disable_harvesting()
             $HUD/HudContainer/PhasePanel/PanelContainer/VBoxContainer/PhaseGather.self_modulate = gather_color_inactive
             $HUD/HudContainer/PhasePanel/PanelContainer/VBoxContainer/PhaseExecute.self_modulate = execute_color_inactive
             $HUD/HudContainer/PhasePanel/PanelContainer/VBoxContainer/PhaseDefend.self_modulate = defend_color_active
+            end_turn_button.disabled = true
+            build_menu_container.visible = false
 
 func on_strength_changed():
     $HUD/HudContainer/StrengthPanel/PanelContainer/GridContainer/ShipsDisplay.text = str(game_state.ships)
@@ -129,13 +148,23 @@ func on_workers_changed(worker_type: int, new_value: int):
         Constants.LB_ADMIN:
             $HUD/HudContainer/WorkersPanel/PanelContainer/GridContainer/AdminDisplay.text = str(new_value)
 
-
 func _on_EndTurnButton_pressed():
     match game_state.current_phase:
         Constants.PHASE_GATHER:
             game_state.current_phase = Constants.PHASE_EXECUTE
         Constants.PHASE_EXECUTE:
-            end_turn_button.disabled = true
             game_state.current_phase = Constants.PHASE_DEFEND
         Constants.PHASE_DEFEND:
             game_state.current_phase = Constants.PHASE_GATHER
+
+func unfreeze_end_turn_button():
+    yield(get_tree().create_timer(1.0), "timeout")
+    end_turn_button.disabled = false
+
+
+func _on_BuildShipButton_pressed():
+    if can_build_ship():
+        game_state.labor_available -= Constants.SHIP_COST[0]
+        game_state.ai_available -= Constants.SHIP_COST[1]
+        game_state.admin_available -= Constants.SHIP_COST[2]
+        game_state.ships += 1
