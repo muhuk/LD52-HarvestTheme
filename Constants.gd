8471220047aaa7f52418b99b8f1a081ab3bbd8c9
Atamert Ölçgen <muhuk@muhuk.com>
2023-01-08 03:31:16 +0300

    Add Ship.

--- Constants.gd
+++ Constants.gd
@@ -5,5 +5,10 @@ enum {PHASE_GATHER, PHASE_EXECUTE, PHASE_DEFEND}
 
 enum {LB_LABOR, LB_AI, LB_ADMIN}
 
+const ENEMY_SHIPS: Array = [0, 1, 1, 2, 3, 5, 8, 13]
 const POPULATION_GROWTH_CHANCE: float = 0.5
 const SHIP_COST: Array = [5, 3, 2]
+
+const SHIP_HP: int = 1000
+const SHIP_HIT_CHANCE: float = 0.6
+const SHIP_DAMAGE: int = 50
